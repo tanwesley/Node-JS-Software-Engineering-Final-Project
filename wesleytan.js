de98a01c691cfbcc33fc349afe0c85c7e2bda2d1
unknown <tanwesley88@gmail.com>
2020-11-18 14:36:41 -0600

    changed routes

--- wesleytan.js
+++ wesleytan.js
@@ -14,36 +14,36 @@ app.use(express.static(__dirname + '/public'))
 
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.render('home'))
+app.get('/', (req, res) => res.render('home.handlebars'))
 
 // syllabus
 app.get('/syllabus', (req, res) => {
-    res.render('syllabus')
+    res.render('syllabus.handlebars')
 })
 
 app.use(express.static(__dirname + '/public'))
 
 // diceroll
 app.get('/diceroll', (req, res) => {
-    res.render('diceroll')
+    res.render('diceroll.handlebars')
 })
 
 // meadowlark
 app.get('/meadowlark', (req, res) => {
-    res.render('meadowlark')
+    res.render('meadowlark.handlebars')
 })
 
 // custom 404 page
 app.use((req,res) => {
     res.status(404)
-    res.render('404')
+    res.render('404.handlebars')
 })
 
 //custom 500 page
 app.use((err, req, res, next) => {
     console.error(err.message)
     res.status(500)
-    res.render('500')
+    res.render('500.handlebars')
 })
 
 app.listen(port, () => console.log(
